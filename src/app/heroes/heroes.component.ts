504e6ffbc5c76fb97e64407fdc3851365de8e216
弦間由樹 <gemma.naok@gmail.com>
2023-11-10 21:23:38 +0900

    ナビゲーション追加

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,10 +22,6 @@ export class HeroesComponent implements OnInit {
   ngOnInit(): void {
     this.getHeroes();
   }
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
