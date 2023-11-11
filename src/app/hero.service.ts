746857efa0e7012d8db205f10b0a5be693915eb9
弦間由樹 <gemma.naok@gmail.com>
2023-11-12 06:39:03 +0900

    HTTP-POST機能追加

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -58,4 +58,11 @@ export class HeroService {
         catchError(this.handleError<any>('updateHero'))
       );
   }
+  /** POST: サーバーに新しいヒーローを登録する */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
